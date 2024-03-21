571ea24d23854646eec68e3847657aab59e1ccce
tite peko <andreinuguid02@gmail.com>
2024-03-21 15:15:59 +0800

    overflow test

--- src/pages/Requests.jsx
+++ src/pages/Requests.jsx
@@ -206,8 +206,8 @@ const Requests = () => {
           </div>
 
           {/* Table */}
-          <div className="sm:w-90% sm:max-h-[380px] lg:max-h-[680px] border border-b-0 mt-5 rounded-t-xl bg-white">
-            <table className="overflow-hidden overflow-x-auto overflow-y-auto w-full divide-y divide-gray-200">
+          <div className="overflow-x-auto sm:w-90% sm:max-h-[380px] lg:max-h-[680px] border border-b-0 mt-5 rounded-t-xl bg-white">
+            <table className="w-full divide-y divide-gray-200">
               <thead className={`bg-[${info && info.theme && info.theme.primary !== "" ? info.theme.primary : "#295141"}] border`}>
                 <tr>
                   {tableHeader.map((item, i) => (
